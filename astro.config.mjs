47d37db6bbc459e47c18e39edd5a93d375a3bbdb
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-23 13:37:38 -0600

    chore: remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  // integrations: [qwikdev(), tailwind()],
-  integrations: [
-    qwikdev(),
-    react({
-      include: ['**/react/*'],
-    }),
-    tailwind(),
-  ],
+  integrations: [qwikdev(), tailwind()],
 });
