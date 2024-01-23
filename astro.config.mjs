8602a3d3edd50514d0e18f763448d7115ac60e14
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-23 13:55:27 -0600

    chore: move react components out of src

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [qwikdev(), tailwind()],
 });
