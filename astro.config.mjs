2486f524dc3ad07fafcb3b63fe39350cc38bb1c9
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-25 09:30:39 -0600

    test: add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), tailwind()],
+  integrations: [
+    tailwind(),
+    react({
+      include: ['**/react/*'],
+    }),
+    qwikdev(),
+  ],
 });
